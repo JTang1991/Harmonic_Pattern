9cbfa20c0f8f89685af4bec9e5e9ce3df86effdd
JTang1991 <59817565+JTang1991@users.noreply.github.com>
2020-08-27 11:16:22 +0800

    Add files via upload

---
+++ backtesting_app.py
@@ -0,0 +1,11 @@
+import sys
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import plot, scatter, show
+
+
+## Get Data
+GBPUSD = pd.read_csv('')
+
